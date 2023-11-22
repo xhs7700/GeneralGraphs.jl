07452c65c167896e3e64e864726e8d7fdd3c6995
xhs7700 <xhs7700@126.com>
2023-11-22 18:09:55 +0800

    add impl of directed graphs

--- src/GeneralGraphs.jl
+++ src/GeneralGraphs.jl
@@ -8,66 +8,49 @@ using LinearAlgebra
 using SparseArrays
 
 export AbstractGraph, GeneralGraph, NormalGraph, NormalWeightedGraph, NormalUnweightedGraph
+export NormalDiGraph, GeneralDiGraph, NormalWeightedDiGraph, NormalUnweightedDiGraph
 export num_nodes, num_edges
+export add_node!, add_edge!
 export diagadj
 export LCC, LCC!
+export LSCC, LSCC!
 
 abstract type AbstractGraph end
 abstract type NormalGraph <: AbstractGraph end
+abstract type NormalDiGraph <: AbstractGraph end
 
 struct GeneralGraph{T<:Real} <: AbstractGraph
     name::AbstractString
     nodes::Set{Int}
     edges::Dict{Tuple{Int,Int},T}
     GeneralGraph{T}() where {T<:Real} = new{T}("", Set{Int}(), Dict{Tuple{Int,Int},T}())
+    GeneralGraph{T}(name::AbstractString) where {T<:Real} = new{T}(name, Set{Int}(), Dict{Tuple{Int,Int},T}())
     GeneralGraph(name::AbstractString, nodes::Set{Int}, edges::Dict{Tuple{Int,Int},T}) where {T<:Real} = new{T}(name, nodes, edges)
     function GeneralGraph(name::AbstractString, source::Union{AbstractString,IO})
         println("Reading graph $name...")
-        nodes = Set{Int}()
-        edges = Dict{Tuple{Int,Int},Int}()
+        g = GeneralGraph{Int}(name)
         for line in ProgressBar(eachline(source))
             if line[begin] in "#%"
                 continue
             end
             line = strip(line, ('\t', ' '))
             u, v, w = map(x -> parse(Int, x), split(line, ('\t', ' ', ',')))
-            if u == v
-                continue
-            end
-            if u > v
-                u, v = v, u
-            end
-            if haskey(edges, (u, v))
-                continue
-            end
-            push!(nodes, u, v)
-            edges[(u, v)] = w
+            add_edge!(g, (u, v, w))
         end
-        new{Int}(name, nodes, edges)
+        g
     end
     function GeneralGraph{T}(generator::Function, name::AbstractString, source::Union{AbstractString,IO}) where {T<:Real}
         println("Reading graph $name...")
-        nodes = Set{Int}()
-        edges = Dict{Tuple{Int,Int},T}()
+        g = GeneralGraph{Int}(name)
         for line in ProgressBar(eachline(source))
             if line[begin] in "#%"
                 continue
             end
             line = strip(line, ('\t', ' '))
             u, v = map(x -> parse(Int, x), split(line, ('\t', ' ', ',')))
-            if u == v
-                continue
-            end
-            if u > v
-                u, v = v, u
-            end
-            if haskey(edges, (u, v))
-                continue
-            end
-            push!(nodes, u, v)
-            edges[(u, v)] = generator()
+            add_edge!(g, (u, v, generator()))
         end
-        new{T}(name, nodes, edges)
+        g
     end
 end
 
@@ -75,6 +58,21 @@ Base.size(g::AbstractGraph) = (num_nodes(g), num_edges(g))
 num_nodes(g::GeneralGraph) = length(g.nodes)
 num_edges(g::GeneralGraph) = length(g.edges)
 prefix(::GeneralGraph) = "GeneralUndiGraph"
+
+add_node!(g::GeneralGraph, u::Int) = push!(g.nodes, u)
+function add_edge!(g::GeneralGraph{T}, (u, v, w)::Tuple{Int,Int,T}) where {T<:Real}
+    if u == v
+        return
+    end
+    if u > v
+        u, v = v, u
+    end
+    if haskey(g.edges, (u, v))
+        return
+    end
+    push!(g.nodes, u, v)
+    g.edges[(u, v)] = w
+end
 
 function LCC(g::GeneralGraph)
     println("Finding LCC of graph $(g.name)...")
@@ -124,6 +122,144 @@ function Base.write(io::IO, g::GeneralGraph)
     write(io, "# $(prefix(g)): $(g.name)\n# Nodes: $n Edges: $m\n")
     for (u, v) in g.edges |> keys |> collect |> sort |> ProgressBar
         write(io, "$u\t$v\t$(g.edges[(u,v)])\n")
+    end
+end
+
+struct GeneralDiGraph{T<:Real} <: AbstractGraph
+    name::AbstractString
+    adjs::Dict{Int,Vector{Tuple{Int,T}}}
+    edges::Dict{Tuple{Int,Int},T}
+    GeneralDiGraph{T}() where {T<:Real} = new{T}("", Dict{Int,Vector{Tuple{Int,T}}}(), Dict{Tuple{Int,Int},T}())
+    GeneralDiGraph{T}(name::AbstractString) where {T<:Real} = new{T}(name, Dict{Int,Vector{Tuple{Int,T}}}(), Dict{Tuple{Int,Int},T}())
+    GeneralDiGraph{T}(name::AbstractString, adjs::Dict{Int,Vector{Tuple{Int,T}}}, edges::Dict{Tuple{Int,Int},T}) where {T<:Real} = new{T}(name, adjs, edges)
+    function GeneralDiGraph(name::AbstractString, source::Union{AbstractString,IO})
+        println("Reading directed graph $name...")
+        g = GeneralDiGraph{Int}(name)
+        for line in ProgressBar(eachline(source))
+            if line[begin] in "#%"
+                continue
+            end
+            line = strip(line, ('\t', ' '))
+            u, v, w = map(x -> parse(Int, x), split(line, ('\t', ' ', ',')))
+            add_edge!(g, (u, v, w))
+        end
+        g
+    end
+    function GeneralDiGraph{T}(generator::Function, name::AbstractString, source::Union{AbstractString,IO}) where {T<:Real}
+        println("Reading directed graph $name...")
+        g = GeneralDiGraph{Int}(name)
+        for line in ProgressBar(eachline(source))
+            if line[begin] in "#%"
+                continue
+            end
+            line = strip(line, ('\t', ' '))
+            u, v = map(x -> parse(Int, x), split(line, ('\t', ' ', ',')))
+            add_edge!(g, (u, v, generator()))
+        end
+        g
+    end
+end
+
+num_nodes(g::GeneralDiGraph) = length(g.adjs)
+num_edges(g::GeneralDiGraph) = length(g.edges)
+prefix(::GeneralDiGraph) = "GeneralDiGraph"
+
+function add_node!(g::GeneralDiGraph{T}, u::Int) where {T<:Real}
+    if haskey(g.adjs, u)
+        return
+    end
+    g.adjs[u] = Tuple{Int,T}[]
+end
+function add_edge!(g::GeneralDiGraph{T}, (u, v, w)::Tuple{Int,Int,T}) where {T<:Real}
+    if u == v || haskey(g.edges, (u, v))
+        return
+    end
+    add_node!(g, u), add_node!(g, v)
+    push!(g.adjs[u], (v, w))
+    g.edges[(u, v)] = w
+end
+
+function Base.write(io::IO, g::GeneralDiGraph)
+    n, m = size(g)
+    write(io, "# $(prefix(g)): $(g.name)\n# Nodes: $n Edges: $m\n")
+    for (u, v) in g.edges |> keys |> collect |> sort |> ProgressBar
+        write(io, "$u\t$v\t$(g.edges[(u,v)])\n")
+    end
+end
+
+function get_lscc_info(g::GeneralDiGraph)
+    println("Finding LSCC of directed graph $(g.name)...")
+    n = num_nodes(g)
+    dfn, low = Dict{Int,Int}(), Dict{Int,Int}()
+    scc_id = Dict{Int,Int}()
+    in_stk, vis = Set{Int}(), Set{Int}()
+    stk, scc_size = Int[], Int[]
+    dfn_cnt = 0
+    scc_cnt = 0
+    sizehint!(dfn, n), sizehint!(low, n)
+    sizehint!(vis, n), sizehint!(scc_id, n)
+    function tarjan(u::Int)
+        push!(vis, u)
+        dfn_cnt += 1
+        dfn[u] = low[u] = dfn_cnt
+        push!(stk, u)
+        push!(in_stk, u)
+        for (v, _) in g.adjs[u]
+            if !(v in vis)
+                tarjan(v)
+                low[u] = min(low[u], low[v])
+            elseif v in in_stk
+                low[u] = min(low[u], dfn[v])
+            end
+        end
+        if dfn[u] == low[u]
+            scc_cnt += 1
+            push!(scc_size, 0)
+            while true
+                v = pop!(stk)
+                delete!(in_stk, v)
+                scc_id[v] = scc_cnt
+                scc_size[scc_cnt] += 1
+                if v == u
+                    break
+                end
+            end
+        end
+    end
+    for u in keys(g.adjs)
+        if !(u in vis)
+            tarjan(u)
+        end
+    end
+    lscc_id = argmax(scc_size)
+    (lscc_id, scc_id)
+end
+
+function LSCC(g::GeneralDiGraph{T}) where {T<:Real}
+    lscc_id, scc_id = get_lscc_info(g)
+    new_name = "$(g.name)_LSCC"
+    new_adjs = Dict{Int,Vector{Tuple{Int,T}}}()
+    for u in keys(g.adjs)
+        if scc_id[u] == lscc_id
+            new_adjs[u] = filter(t -> scc_id[t[1]] == lscc_id, g.adjs[u])
+        end
+    end
+    new_edges = filter(g.edges) do p
+        u, v = p.first
+        scc_id[u] == lscc_id && scc_id[v] == lscc_id
+    end
+    GeneralDiGraph{T}(new_name, new_adjs, new_edges)
+end
+
+function LSCC!(g::GeneralDiGraph)
+    lscc_id, scc_id = get_lscc_info(g)
+    filter!(p -> scc_id[p.first] == lscc_id, g.adjs)
+    for u in keys(g.adjs)
+        filter!(t -> scc_id[t[1]] == lscc_id, g.adjs[u])
+    end
+    filter!(g.edges) do p
+        u, v = p.first
+        scc_id[u] == lscc_id && scc_id[v] == lscc_id
     end
 end
 
@@ -290,5 +426,123 @@ function diagadj(::Type{T}, g::NormalUnweightedGraph) where {T<:Real}
 end
 
 diagadj(g::NormalUnweightedGraph) = diagadj(Float64, g)
+
+struct NormalWeightedDiGraph{T<:Real} <: NormalDiGraph
+    n::Int
+    m::Int
+    name::AbstractString
+    adjs::Vector{Vector{Int}}
+    weights::Vector{Vector{T}}
+    normalized_weights::Vector{ProbabilityWeights}
+    function NormalWeightedDiGraph(g::GeneralDiGraph{T}) where {T<:Real}
+        n = num_nodes(g)
+        o2n = DefaultDict{Int,Int}(() -> length(o2n) + 1)
+        adjs = Vector{Vector{Int}}(undef, n)
+        weights = Vector{Vector{T}}(undef, n)
+        renumber = maximum(keys(g.adjs); init=0) != n
+        if renumber
+            for u in ProgressBar(keys(g.adjs))
+                new_u = o2n[u]
+                adjs[new_u] = map(t -> o2n[t[1]], g.adjs[u])
+                weights[new_u] = map(t -> t[2], g.adjs[u])
+            end
+        else
+            for u in ProgressBar(keys(g.adjs))
+                adjs[u] = map(t -> t[1], g.adjs[u])
+                weights[u] = map(t -> t[2], g.adjs[u])
+            end
+        end
+        for i in (1:n)
+            p = sortperm(adjs[i])
+            permute!(adjs[i], p), permute!(weights[i], p)
+        end
+        normalized_weights = [ProbabilityWeights(normalize(weights[u], 1)) for u in (1:n)]
+        new{T}(n, length(g.edges), g.name, adjs, weights, normalized_weights)
+    end
+    NormalWeightedDiGraph(name::AbstractString, source::Union{AbstractString,IO}) = NormalWeightedDiGraph(GeneralDiGraph(name, source))
+    NormalWeightedDiGraph{T}(generator::Function, name::AbstractString, source::Union{AbstractString,IO}) where {T<:Real} = NormalWeightedDiGraph(GeneralDiGraph{T}(generator, name, source))
+end
+
+num_nodes(g::NormalWeightedDiGraph) = g.n
+num_edges(g::NormalWeightedDiGraph) = g.m
+prefix(::NormalWeightedDiGraph) = "WeightedDiGraph"
+
+function Base.write(io::IO, g::NormalWeightedDiGraph)
+    write(io, "# $(prefix(g)): $(g.name)\n# Nodes: $(g.n) Edges: $(g.m)\n")
+    for u in 1:g.n
+        for (v, w) in zip(g.adjs[u], g.weights[u])
+            write(io, "$u\t$v\t$w\n")
+        end
+    end
+end
+
+function diagadj(g::NormalWeightedDiGraph{T}) where {T<:Real}
+    n, m = g.n, g.m
+    A_I, A_J, A_V = Int[], Int[], T[]
+    sizehint!(A_I, m), sizehint!(A_J, m), sizehint!(A_V, m)
+    d = Vector{T}(undef, n)
+    for i in 1:n
+        len = length(g.adjs[i])
+        append!(A_I, repeat([i], len)), append!(A_J, g.adjs[i]), append!(A_V, g.weights[i])
+        d[i] = sum(g.weights[i])
+    end
+    return d, sparse(A_I, A_J, A_V, n, n)
+end
+
+struct NormalUnweightedDiGraph
+    n::Int
+    m::Int
+    name::AbstractString
+    adjs::Vector{Vector{Int}}
+    function NormalUnweightedDiGraph(g::GeneralDiGraph)
+        n = num_nodes(g)
+        o2n = DefaultDict{Int,Int}(() -> length(o2n) + 1)
+        adjs = Vector{Vector{Int}}(undef, n)
+        renumber = maximum(keys(g.adjs); init=0) != n
+        if renumber
+            for u in ProgressBar(keys(g.adjs))
+                new_u = o2n[u]
+                adjs[new_u] = map(t -> o2n[t[1]], g.adjs[u])
+                sort!(adjs[new_u])
+            end
+        else
+            for u in ProgressBar(keys(g.adjs))
+                adjs[u] = map(t -> t[1], g.adjs[u])
+                sort!(adjs[u])
+            end
+        end
+        new(n, length(g.edges), g.name, adjs)
+    end
+    NormalUnweightedDiGraph(n::Int, m::Int, name::AbstractString, adjs::Vector{Vector{Int}}) = new(n, m, name, adjs)
+    NormalUnweightedDiGraph(name::AbstractString, source::Union{AbstractString,IO}) = NormalUnweightedDiGraph(GeneralDiGraph{Int}(() -> 1, name, source))
+end
+
+num_nodes(g::NormalUnweightedDiGraph) = g.n
+num_edges(g::NormalUnweightedDiGraph) = g.m
+prefix(::NormalUnweightedDiGraph) = "UnweightedDiGraph"
+
+function Base.write(io::IO, g::NormalUnweightedDiGraph)
+    write(io, "# $(prefix(g)): $(g.name)\n# Nodes: $(g.n) Edges: $(g.m)\n")
+    for u in 1:g.n
+        for v in g.adjs[u]
+            write(io, "$u\t$v\n")
+        end
+    end
+end
+
+function diagadj(::Type{T}, g::NormalUnweightedDiGraph) where {T<:Real}
+    n, m = g.n, g.m
+    A_I, A_J = Int[], Int[]
+    sizehint!(A_I, m), sizehint!(A_J, m)
+    d = Vector{T}(undef, n)
+    for i in 1:n
+        len = length(g.adjs[i])
+        append!(A_I, repeat([i], len)), append!(A_J, g.adjs[i])
+        d[i] = len
+    end
+    return d, sparse(A_I, A_J, ones(T, length(A_I)), n, n)
+end
+
+diagadj(g::NormalUnweightedDiGraph) = diagadj(Float64, g)
 
 end # module GeneralGraphs
